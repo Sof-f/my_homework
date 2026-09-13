6fdf1197c0d993792d2c32fd8171ea0638b1f021
Sof-f <sofiakrasnicenko@gmail.com>
2026-09-13 10:47:04 +0300

    hw 2 + triangle

--- homework_2.py
+++ homework_2.py
@@ -13,3 +13,36 @@ elif shape == "square":
 
 elif shape != "circle" and shape != "square":
     print("You entered wrong shape. Please enter circle or square.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a= float(input("Enter side a: "))
+b= float(input("Enter side b: "))
+c= float(input("Enter side c: "))
+
+p= (a+b+c)/2
+s= math.sqrt(p*(p-a)*(p-b)*(p-c))
+print(s)
